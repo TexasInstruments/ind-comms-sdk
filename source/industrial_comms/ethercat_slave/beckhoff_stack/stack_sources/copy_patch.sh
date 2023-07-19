8b5bd3e05b63959cee3542f3fb92ddb5ab6958fc
Naresh A <nareshk@ti.com>
2023-07-19 16:50:58 +0530

    am64x/am243x/am263x : remove unwanted shell scripts from Beckhoff stack_sources folder
    Remove unwanted shell scripts from Beckhoff stack_sources folder
    
    Fixes: PINDSW-5787
    
    Signed-off-by: Naresh A <nareshk@ti.com>

--- source/industrial_comms/ethercat_slave/beckhoff_stack/stack_sources/copy_patch.sh
+++
@@ -1,6 +0,0 @@
-PINDSW_REPO_PATH=/c/d/industrial_communications_git/industrial_communications_dev/
-
-cp TI_ECAT.patch $PINDSW_REPO_PATH/public/sdk/third_party/protocols/ethercat_slave/patch/TI_ECAT.patch
-cp 8SM_TI_ECAT.patch $PINDSW_REPO_PATH/ti_internal/engineering/test_patches/ethercat_slave/8SM_test_app/8SM_TI_ECAT.patch
-cp TI_ECAT_DEMO.patch $PINDSW_REPO_PATH/ti_internal/protocols/ethercat_slave/host_stack/sys_bios/ecat_slave_stack/TI_ECAT_DEMO.patch
-
