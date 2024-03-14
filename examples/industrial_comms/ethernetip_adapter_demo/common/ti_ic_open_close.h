528c69c5efa8c7a2fe2d9583f8bbfe08a4bede94
Nitika Verma <nitika.verma@ti.com>
2024-03-14 16:50:55 +0530

    Adding bridge config files

---
+++ examples/industrial_comms/ethernetip_adapter_demo/common/ti_ic_open_close.h
@@ -0,0 +1,23 @@
+//ti_ic_lwipif.h
+
+#ifndef TI_IC_OPEN_CLOSE_H_
+#define TI_IC_OPEN_CLOSE_H_
+/* ========================================================================== */
+/*                             Include Files                                  */
+/* ========================================================================== */
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <intercore/intercore.h>
+#include <lwip_ic.h>
+#include <intercore/include/ShdMemCircularBufferP_nortos.h>
+#include "app_control.h"
+
+Ic_Object_Handle App_doIcOpen(uint32_t instId);
+
+int32_t App_getSharedMemInfo(Icve_respMsg *respMsg, uint32_t remoteCoreId);
+extern uint32_t App_getSelfCoreId();
+#ifdef __cplusplus
+}
+#endif
+#endif /* TI_IC_OPEN_CLOSE_H_ */
