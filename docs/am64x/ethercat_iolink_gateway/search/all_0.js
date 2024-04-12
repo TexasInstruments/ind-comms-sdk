dd7d92f139a09a08cac712daadf48e7ee0bb6582
Naresh A <nareshk@ti.com>
2024-04-12 13:20:00 +0530

    am64x/am243x : ecat-iolink : integrate EtherCAT-IOLink GW docs v3.02.00
    Integrate EtherCAT-IOLink Gateway v3.02.00 documentation for v9.2
    
    Fixes: PINDSW-7710
    
    Signed-off-by: Naresh A <nareshk@ti.com>

--- docs/am64x/ethercat_iolink_gateway/search/all_0.js
+++ docs/am64x/ethercat_iolink_gateway/search/all_0.js
@@ -1,14 +1,12 @@
 var searchData=
 [
-  ['00_0',['00',['../releasenotes.html#version_release_020100',1,'Release Version 2.01.00'],['../releasenotes.html#version_release_020200',1,'Release Version 2.02.00']]],
+  ['00_0',['00',['../releasenotes.html#version_release_020100',1,'Release Version 2.01.00'],['../releasenotes.html#version_release_020200',1,'Release Version 2.02.00'],['../releasenotes.html#version_release_030100',1,'Release Version 3.01.00'],['../releasenotes.html#version_release_030200',1,'Release Version 3.02.00']]],
   ['00_2000_1',['00 00',['../releasenotes.html#version_release_010000',1,'Release Version 1.00.00'],['../releasenotes.html#version_release_020000',1,'Release Version 2.00.00']]],
   ['00_2001_2',['00 01',['../releasenotes.html#version_release_010001',1,'Release Version 1.00.01'],['../releasenotes.html#version_release_030001',1,'Release Version 3.00.01']]],
   ['00_2002_3',['Release Version 3.00.02',['../releasenotes.html#version_release_030002',1,'']]],
-  ['00_20to_20be_20released_4',['Release Version 3.01.00 (to be released)',['../releasenotes.html#version_release_030100',1,'']]],
-  ['01_5',['01',['../releasenotes.html#version_release_010001',1,'Release Version 1.00.01'],['../releasenotes.html#version_release_030001',1,'Release Version 3.00.01']]],
-  ['01_2000_6',['Release Version 2.01.00',['../releasenotes.html#version_release_020100',1,'']]],
-  ['01_2000_20to_20be_20released_7',['Release Version 3.01.00 (to be released)',['../releasenotes.html#version_release_030100',1,'']]],
-  ['01_2001_8',['Release Version 2.01.01',['../releasenotes.html#version_release_020101',1,'']]],
-  ['02_9',['Release Version 3.00.02',['../releasenotes.html#version_release_030002',1,'']]],
-  ['02_2000_10',['Release Version 2.02.00',['../releasenotes.html#version_release_020200',1,'']]]
+  ['01_4',['01',['../releasenotes.html#version_release_010001',1,'Release Version 1.00.01'],['../releasenotes.html#version_release_030001',1,'Release Version 3.00.01']]],
+  ['01_2000_5',['01 00',['../releasenotes.html#version_release_020100',1,'Release Version 2.01.00'],['../releasenotes.html#version_release_030100',1,'Release Version 3.01.00']]],
+  ['01_2001_6',['01 01',['../releasenotes.html#version_release_020101',1,'Release Version 2.01.01'],['../releasenotes.html#version_release_030101',1,'Release Version 3.01.01']]],
+  ['02_7',['Release Version 3.00.02',['../releasenotes.html#version_release_030002',1,'']]],
+  ['02_2000_8',['02 00',['../releasenotes.html#version_release_020200',1,'Release Version 2.02.00'],['../releasenotes.html#version_release_030200',1,'Release Version 3.02.00']]]
 ];
--- docs/am64x/ethercat_iolink_gateway/search/all_15.js
+++ docs/am64x/ethercat_iolink_gateway/search/all_15.js
@@ -5,12 +5,11 @@ var searchData=
   ['the_20example_2',['Steps to Run the Example',['../page_quickstart_installation.html#qs_steps_to_run_example',1,'']]],
   ['the_20examples_20with_20ccs_3',['the examples with ccs',['../example_extended.html#run-code-composer-user',1,'Run and debug the examples with CCS'],['../example.html#run-code-composer-user-basicExPage',1,'Run and debug the examples with CCS']]],
   ['the_20twincat_20projects_4',['Run the TwinCAT projects',['../example_extended.html#run-twincat-user',1,'']]],
-  ['to_20be_20released_5',['Release Version 3.01.00 (to be released)',['../releasenotes.html#version_release_030100',1,'']]],
-  ['to_20read_20write_20data_20from_20to_20an_20io_20link_20device_20using_20twincat_6',['Steps to Read/Write Data from/to an IO-Link Device using TwinCAT',['../twincat_data_rw.html',1,'']]],
-  ['to_20run_20the_20example_7',['Steps to Run the Example',['../page_quickstart_installation.html#qs_steps_to_run_example',1,'']]],
-  ['twincat_8',['Steps to Read/Write Data from/to an IO-Link Device using TwinCAT',['../twincat_data_rw.html',1,'']]],
-  ['twincat_20basic_20project_9',['Start TwinCAT - Basic Project',['../twincat.html',1,'']]],
-  ['twincat_20extended_20project_10',['Start TwinCAT - Extended Project',['../twincat_extended.html',1,'']]],
-  ['twincat_20project_11',['Create a TwinCAT project',['../example.html#run-twincat-user-basicExPage',1,'']]],
-  ['twincat_20projects_12',['Run the TwinCAT projects',['../example_extended.html#run-twincat-user',1,'']]]
+  ['to_20read_20write_20data_20from_20to_20an_20io_20link_20device_20using_20twincat_5',['Steps to Read/Write Data from/to an IO-Link Device using TwinCAT',['../twincat_data_rw.html',1,'']]],
+  ['to_20run_20the_20example_6',['Steps to Run the Example',['../page_quickstart_installation.html#qs_steps_to_run_example',1,'']]],
+  ['twincat_7',['Steps to Read/Write Data from/to an IO-Link Device using TwinCAT',['../twincat_data_rw.html',1,'']]],
+  ['twincat_20basic_20project_8',['Start TwinCAT - Basic Project',['../twincat.html',1,'']]],
+  ['twincat_20extended_20project_9',['Start TwinCAT - Extended Project',['../twincat_extended.html',1,'']]],
+  ['twincat_20project_10',['Create a TwinCAT project',['../example.html#run-twincat-user-basicExPage',1,'']]],
+  ['twincat_20projects_11',['Run the TwinCAT projects',['../example_extended.html#run-twincat-user',1,'']]]
 ];
