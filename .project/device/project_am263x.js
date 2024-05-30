cb4923cc35b9ffe9b082f8dc0e85eecbacd35b31
Aaron <aaron.richard@ti.com>
2024-05-30 10:24:19 +0530

    am263x/am263px: ICSS-EMAC: Remove support
    - Remove ICSS-EMAC driver support in AM263x/AM263Px
    - Remove ICSS-EMAC section in documentation
    
    Fixes: PINDSW-7902
    
    Signed-off-by: Aaron Thomas <aaron.richard@ti.com>

--- .project/device/project_am263x.js
+++ .project/device/project_am263x.js
@@ -2,7 +2,6 @@ const common = require("../common.js");
 
 const component_file_list = [
     "source/industrial_comms/ethercat_slave/icss_fwhal/.project/project.js",
-    "source/networking/icss_emac/.project/project.js",
     "docs_src/docs/api_guide/doxy_samples/.project/project.js",
 ];
 
