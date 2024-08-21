6cb07650c978ad9787e6f2bb85fa2ef3824e1415
Laxman <l-chinnannavar@ti.com>
2024-08-21 19:41:48 +0530

    am243x/am64x: PROFINET: Segregate IRT and RT FW.
    - Minor changes on IRT FW for better performance.
    
    Fixes: PINDSW-5457
    
    Signed-off-by: Laxman <l-chinnannavar@ti.com>

--- source/industrial_comms/profinet_device/icss_fwhal/firmware/mii/firmware_version.h
+++ source/industrial_comms/profinet_device/icss_fwhal/firmware/mii/firmware_version.h
@@ -69,7 +69,7 @@ extern "C"
 // bit23..16		minor version number
 #define		FIRMWARE_VERSION_MINOR			0x13
 // bit15..0		build number
-#define		FIRMWARE_VERSION_BUILD			0x00
+#define		FIRMWARE_VERSION_BUILD			0x08
 
 
 
